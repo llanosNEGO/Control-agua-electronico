b3666c83c7d02a52620578a5aabde094dc34819d
Abed Llovera <negolloverallanos@gmail.com>
2024-11-13 12:19:25 -0500

    termine ultimas actualizaciones

--- web/control.py
+++ web/control.py
@@ -4,18 +4,16 @@ import time
 
 app = Flask(__name__)
 
-# Función para abrir el puerto serial
 def open_serial_port():
     try:
         motor_serial = serial.Serial('COM3', 9600)
-        time.sleep(2)  # Esperar un momento para asegurar que el puerto esté listo
+        time.sleep(2)  
         return motor_serial
     except serial.SerialException as e:
         print(f"Error al abrir el puerto serial: {e}")
         return None
 
-# Inicializamos la variable de estado del motor
-motor_status = "off"  # Puede ser 'on' o 'off'
+motor_status = "off" 
 
 @app.route('/')
 def index():
@@ -25,21 +23,19 @@ def index():
 def control_motor():
     global motor_status
 
-    # Intentamos abrir el puerto serial en cada solicitud
     motor_serial = open_serial_port()
 
-    # Si el puerto se abre correctamente, procedemos con la acción
     if motor_serial and motor_serial.is_open:
         comando = request.form.get('action')
 
-        if comando == '1':  # Encender el motor
-            motor_serial.write(b'1')  # Enviar el comando para encender
+        if comando == '1':  
+            motor_serial.write(b'1')  
             motor_status = 'on'
-        elif comando == 'A':  # Apagar el motor
-            motor_serial.write(b'A')  # Enviar el comando para apagar
+        elif comando == 'A': 
+            motor_serial.write(b'A')  
             motor_status = 'off'
         
-        motor_serial.close()  # Cerrar el puerto después de la acción
+        motor_serial.close() 
         return jsonify({"status": f"Motor {'encendido' if motor_status == 'on' else 'apagado'}"}), 200
     else:
         return jsonify({'status': 'Error de conexión serial'}), 500
